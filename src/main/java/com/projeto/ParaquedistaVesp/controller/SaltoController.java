cd00a4e611a1d2e0aadf9f0c9c833653917d6b8b
narciza-fialho <mfialho@itelinfo.com>
2021-12-16 15:34:08 -0400

    salto repository e controller

---
+++ src/main/java/com/projeto/ParaquedistaVesp/controller/SaltoController.java
@@ -0,0 +1,4 @@
+package com.projeto.ParaquedistaVesp.controller;
+
+public class SaltoController {
+}
---
+++ src/main/java/com/projeto/ParaquedistaVesp/repositories/SaltoRepository.java
@@ -0,0 +1,4 @@
+package com.projeto.ParaquedistaVesp.repositories;
+
+public class SaltoRepository {
+}
