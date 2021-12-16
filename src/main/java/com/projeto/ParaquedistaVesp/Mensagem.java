2c82e7c4fe89da0a0f10828829470d304936d45f
narciza-fialho <mfialho@itelinfo.com>
2021-12-16 15:23:09 -0400

    configurações

---
+++ src/main/java/com/projeto/ParaquedistaVesp/Mensagem.java
@@ -0,0 +1,13 @@
+package com.projeto.ParaquedistaVesp;
+
+public class Mensagem {
+    private String mensagem;
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
