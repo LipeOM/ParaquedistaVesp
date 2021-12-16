56af154d05c6f39995083eab9fe99fd4b981a810
narciza-fialho <mfialho@itelinfo.com>
2021-12-16 15:42:35 -0400

    repository salto

--- src/main/java/com/projeto/ParaquedistaVesp/repositories/SaltoRepository.java
+++ src/main/java/com/projeto/ParaquedistaVesp/repositories/SaltoRepository.java
@@ -1,4 +1,12 @@
 package com.projeto.ParaquedistaVesp.repositories;
 
-public interface SaltoRepository {
+import com.projeto.ParaquedistaVesp.entities.Salto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SaltoRepository extends JpaRepository<Salto, Integer> {
+    List<Salto> findByAtivo(Boolean ativo);
 }
