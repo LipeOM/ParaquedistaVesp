bbfe01b832a8f4593b84cab424d73beb0acdcb54
narciza-fialho <mfialho@itelinfo.com>
2021-12-16 15:17:12 -0400

    primeiro commit

---
+++ src/main/java/com/projeto/ParaquedistaVesp/ParaquedistaVespApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ParaquedistaVesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaquedistaVespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaquedistaVespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/ParaquedistaVesp/ParaquedistaVespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ParaquedistaVesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParaquedistaVespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
