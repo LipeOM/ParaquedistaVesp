5e18fddf8b6cfcf7023bd1928c38f54ab1b2b397
narciza-fialho <mfialho@itelinfo.com>
2021-12-16 15:50:12 -0400

    controller salto

--- src/main/java/com/projeto/ParaquedistaVesp/controller/SaltoController.java
+++ src/main/java/com/projeto/ParaquedistaVesp/controller/SaltoController.java
@@ -1,4 +1,30 @@
 package com.projeto.ParaquedistaVesp.controller;
 
+import com.projeto.ParaquedistaVesp.entities.Salto;
+import com.projeto.ParaquedistaVesp.repositories.SaltoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("salto")
 public class SaltoController {
+    @Autowired
+    private SaltoRepository saltoRepository;
+
+    @GetMapping
+    public List<Salto> listar () {
+        List<Salto> lista = saltoRepository.findByAtivo(true);
+        return lista;
+    }
+
+    @GetMapping("/{id}")
+    public Salto buscar (@PathVariable int id) {
+        Salto salto = saltoRepository.findById(id).get();
+        return salto;
+    }
 }
