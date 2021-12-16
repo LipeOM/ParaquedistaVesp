7bb82db982473cb9ce99aefc370770b063058597
enzookita <eokita@itelinfo.com>
2021-12-16 16:00:27 -0400

    repositoryBatalh21

---
+++ src/main/java/com/projeto/ParaquedistaVesp/controller/BatalhaController.java
@@ -0,0 +1,46 @@
+package com.projeto.ParaquedistaVesp.controller;
+
+
+import com.projeto.ParaquedistaVesp.Mensagem;
+import com.projeto.ParaquedistaVesp.entities.Batalha;
+import com.projeto.ParaquedistaVesp.repositories.BatalhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("batalha")
+public class BatalhaController {
+
+    @Autowired
+    private BatalhaRepository batalhaRepository;
+
+    @GetMapping
+    public List<Batalha> lista(){
+        List<Batalha> lista = batalhaRepository.findByativo(true);
+        return lista;
+    }
+
+    @GetMapping("/{id}")
+    public Batalha buscar(@PathVariable int id){
+        Batalha batalha = batalhaRepository.findById(id).get();
+        return batalha;
+    }
+
+    @PostMapping
+    public Mensagem incluir(@RequestBody Batalha batalha){
+
+        batalha.setId(0);
+        batalhaRepository.saveAndFlush(batalha);
+        Mensagem msg = new Mensagem();
+        msg.setMensagem("Inserido!!");
+        return msg;
+    }
+
+
+
+
+
+
+}
