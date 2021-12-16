37e4756e0c71b5d8c7a0aaf17f0d6a1da1c2e56b
ViniciusPaulaSouza <vsouza@itelinfo.com>
2021-12-16 16:04:25 -0400

    Incluir Alterar Deletar Cidade

--- src/main/java/com/projeto/ParaquedistaVesp/controller/CidadeConntroller.java
+++ src/main/java/com/projeto/ParaquedistaVesp/controller/CidadeConntroller.java
@@ -1,12 +1,10 @@
 package com.projeto.ParaquedistaVesp.controller;
 
+import com.projeto.ParaquedistaVesp.Mensagem;
 import com.projeto.ParaquedistaVesp.entities.Cidade;
 import com.projeto.ParaquedistaVesp.repositories.CidadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,36 @@ public class CidadeConntroller {
         Cidade cidade = cidadeRepository.findById(id).get();
         return cidade;
     }
+
+    @PostMapping
+    public Mensagem incluir (@RequestBody Cidade cidade){
+        cidade.setId(0);
+        cidadeRepository.save(cidade);
+        cidadeRepository.flush();
+
+        Mensagem msg = new Mensagem();
+        msg.setMensagem("Ok");
+        return msg;
+    }
+
+    @PutMapping
+    public Mensagem alterar (@RequestBody Cidade cidade){
+        cidadeRepository.save(cidade);
+        cidadeRepository.flush();
+
+        Mensagem msg = new Mensagem();
+        msg.setMensagem("ok");
+        return msg;
+    }
+    @DeleteMapping
+    public Mensagem deletar (@RequestBody Cidade cidade){
+        cidade.setAtivo(false);
+        cidadeRepository.save(cidade);
+        cidadeRepository.flush();
+        Mensagem msg = new Mensagem();
+        msg.setMensagem("Deletado");
+        return msg;
+    }
+
 
 }
