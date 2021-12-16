59ac6a28b3305cd6b5b416d60c1928c47cd3d57c
ViniciusPaulaSouza <vsouza@itelinfo.com>
2021-12-16 15:40:55 -0400

    Entidade Cidade

---
+++ src/main/java/com/projeto/ParaquedistaVesp/entities/Cidade.java
@@ -0,0 +1,50 @@
+package com.projeto.ParaquedistaVesp.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="ciadade")
+public class Cidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name="id")
+    private Integer id;
+    @Column (name="nome")
+    private  String nome;
+    @Column (name="estado")
+    private String estado;
+    @Column (name="ativo")
+    private Boolean ativo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
